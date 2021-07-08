209b3de4b7951c8e4e09824c143fd85bd2e24416
rxliuli <rxliuli@gmail.com>
2021-07-08 15:53:00 +0800

    feat: 添加 prettier 格式化配置

--- src/constants/routeList.ts
+++ src/constants/routeList.ts
@@ -1,25 +1,25 @@
-import { RouteConfig } from "react-router-config";
-import { lazy } from "react";
+import { RouteConfig } from 'react-router-config'
+import { lazy } from 'react'
 
 export const routeList: RouteConfig[] = [
   {
-    path: "/",
-    component: lazy(() => import("../pages/home/")),
+    path: '/',
+    component: lazy(() => import('../pages/home/')),
     exact: true,
   },
   {
-    path: "/parent",
-    component: lazy(() => import("../pages/parent/")) as any,
+    path: '/parent',
+    component: lazy(() => import('../pages/parent/')) as any,
     routes: [
       {
-        path: "/parent/",
-        component: lazy(() => import("../pages/parent/page/sub1")),
+        path: '/parent/',
+        component: lazy(() => import('../pages/parent/page/sub1')),
         exact: true,
       },
       {
-        path: "/parent/sub2",
-        component: lazy(() => import("../pages/parent/page/sub2")),
+        path: '/parent/sub2',
+        component: lazy(() => import('../pages/parent/page/sub2')),
       },
     ],
   },
-];
+]
